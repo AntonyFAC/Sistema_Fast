b339630e50dd01677663e41b61fda483665b8167
AntonyFAC <katochiac@gmail.com>
2023-07-20 16:42:53 -0500

    Previsualizaciones

--- core/views.py
+++ core/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
-
+from core import *
 # Create your views here.
+def home(request):
+    return render(request, "core/home.html")
+def service(request):
+    return render(request, "core/service.html")
+def visit(request):
+    return render(request, "core/visit.html")
+def blog(request):
+    return render(request, "core/blog.html")
+def contact(request):
+    return render(request, "core/contact.html")
+def history(request):
+    return render(request, "core/history.html")
+def other(request):
+    return render(request, "core/other.html")
